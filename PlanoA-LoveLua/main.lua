bd2bf5888cc2564f05b9d07dfb37e3c284733d27
Bruno Ribeiro Batista <bruno@engsupport.eti.br>
2024-02-28 14:02:28 -0300

    25/02/2024

--- PlanoA-LoveLua/main.lua
+++ PlanoA-LoveLua/main.lua
@@ -31,12 +31,12 @@ end
 --
 
 function love.mousepressed(x,y)
+  -- Posições em blocos de 50
   local posX, posY = math.floor(x/50), math.floor(y/50)
   
-  if mat[posY][posX] ~= '' then
-    aviso = 'Jogada inválida, clicou em "'..mat[posY][posX]..'" ('..posX..','..posY..')'
-    return
-  else aviso = '' end
+  -- Verificação de jogadas erradas
+  if ((x<50 or x>300) or (y<50 or y>300)) then return end
+  if mat[posY][posX] ~= '' then return end
   
   if (isPlyr) then
     mat[posY][posX] = 'Pl'
@@ -55,30 +55,52 @@ function checkClick(x,y)
   if x == 1 or y == 1 or x == 5 or y == 5 then
     -- Borda esquerda
     if x == 1 then
-      if mat[y][2] ~= 'X' and mat[y][3] ~= '' and mat[y-1][2] ~= '' and mat[y+1][2] ~= '' then marcou(2,y) end
+      if mat[y][2] == 'X' and mat[y][3] ~= '' and mat[y-1][2] ~= '' and mat[y+1][2] ~= '' then marcou(2,y) end
       
     -- Borda superior
-    elseif y == 1 then
-      if mat[2][x] ~= 'X' and mat[3][x] ~= '' and mat[2][x+1] ~= '' and mat[2][x-1] ~= '' then marcou(x,2) end
+  elseif y == 1 then
+      if mat[2][x] == 'X' and mat[3][x] ~= '' and mat[2][x+1] ~= '' and mat[2][x-1] ~= '' then marcou(x,2) end
       
       -- Borda direita
     elseif x == 5 then
-      if mat[y][4] ~= 'X' and mat[y][3] ~= '' and mat[y-1][4] ~= '' and mat[y+1][4] ~= '' then marcou(4,y) end
+      if mat[y][4] == 'X' and mat[y][3] ~= '' and mat[y-1][4] ~= '' and mat[y+1][4] ~= '' then marcou(4,y) end
       
     -- Borda inferior
-    elseif y == 5 then
-      if mat[4][x] ~= 'X' and mat[3][x] ~= '' and mat[4][x+1] ~= '' and mat[4][x-1] ~= '' then marcou(x,4) end
+    else
+      if mat[4][x] == 'X' and mat[3][x] ~= '' and mat[4][x+1] ~= '' and mat[4][x-1] ~= '' then marcou(x,4) end
       
     end
   -- Interior do tabuleiro, checar dois quadrados
   else
-  
+    local temp = isPlyr
+    -- Linha vertical do meio
+    if x == 3 then
+      if mat[y][2] == 'X' and mat[y][1] ~= '' and mat[y-1][2] ~= '' and mat[y+1][2] ~= '' then 
+        marcou(2,y)
+        isPlyr = not isPlyr
+      end
+      if mat[y][4] == 'X' and mat[y][5] ~= '' and mat[y-1][4] ~= '' and mat[y+1][4] ~= '' then
+        marcou(4,y)
+        isPlyr = not isPlyr
+      end
+      
+    -- Linha horizontal do meio
+    else
+      if mat[2][x] == 'X' and mat[1][x] ~= '' and mat[2][x-1] ~= '' and mat[2][x+1] ~= '' then 
+        marcou(x,2)
+        isPlyr = not isPlyr
+      end
+      if mat[4][x] == 'X' and mat[5][x] ~= '' and mat[4][x-1] ~= '' and mat[4][x+1] ~= '' then
+        marcou(x,4)
+        isPlyr = not isPlyr
+      end
+    end
   end
 end
 --
 
 function marcou(x,y)
-  if isPlyr then mat[y][x] = 'Jg'
+  if isPlyr then mat[y][x] = 'Pl'
   else mat[y][x] = 'IA' end
   isPlyr = not isPlyr
 end
